0083c07db3bdfe2b9ddcf970a85732c2fa8762f8
Cleber Santos <cleberson.181@gmail.com>
2018-11-05 23:01:53 -0200

    metodos de lista

--- index.php
+++ index.php
@@ -2,9 +2,31 @@
 
 require_once("config.php");
 
+/* ========= Carrega um usuario ========
+
 $root = new usuario();
 $root->loadbyId(6);
+echo $root;     
+*/
 
-echo $root;
+
+/* ========= Carrega uma lista de usuários =========
+
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+/* ========= Carrega uma lista de usuarios buscandoo pelo login =========
+
+$search = Usuario::search("cl");
+echo json_encode($search)
+*/
+
+/* ========= Carrega usuario usando login e senha ========= */
+
+$usuario = new Usuario();
+$usuario -> login("cleber","santos");
+echo $usuario; 
+
 
  ?>
